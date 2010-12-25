6b11f01d813a7e2d97fb19d62d1c71d85fcad626
Chetan Surpur <chetan51@gmail.com>
2010-12-24 17:54:01 -0800

    Updating live site

---
+++ site/media/js/plugins.js
@@ -0,0 +1,1 @@
+(function(a){})(window.jQuery);window.log=function(){log.history=log.history||[];log.history.push(arguments);if(this.console){console.log(Array.prototype.slice.call(arguments))}};(function(b){var a=b.write;b.write=function(c){log("document.write(): ",arguments);if(/docwriteregexwhitelist/.test(c)){a.apply(b,arguments)}}})(document);
