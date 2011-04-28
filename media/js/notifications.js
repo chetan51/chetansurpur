e0a046475c5ff179fa28d30ca92f801057c4476d
Chetan Surpur <chetan51@gmail.com>
2011-04-28 02:35:58 -0700

    Added notification system

---
+++ media/js/notifications.js
@@ -0,0 +1,14 @@
+$(function(){
+	current_notification_version = 1;
+	
+	if (current_notification_version > localStorage.getItem('chetansurpur_notification')) {
+		$("#site").css("margin-top", parseInt($("#site").css("margin-top").replace("px", "")) + $("#notifications").height());
+		$("#notifications").slideDown();
+	}
+		
+	$("#notifications .close").click(function() {
+		$("#notifications").slideUp();
+		$("#site").css("margin-top", parseInt($("#site").css("margin-top").replace("px", "")) - $("#notifications").height());
+		localStorage.setItem('chetansurpur_notification', current_notification_version);
+	});
+});
